d6992cdc749c3ec8af48bbfdcc204e99e9192dd1
Hannah <hannahvanseeters@gmail.com>
2018-12-14 09:55:40 -0800

    fixed select on form by adding name=emoji plus added emoji logos in select instead of text

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -43,7 +43,7 @@ class NewCardForm extends Component {
 
   render () {
     const populateOptions = EMOJI_LIST.map((option, i) => {
-      return <option key={i} value={option}>{option}</option>
+      return <option key={i} value={option}>{emoji.getUnicode(option)}</option>
     });
 
     return (
@@ -51,15 +51,15 @@ class NewCardForm extends Component {
         <h2 className="new-card-form__header">Add an Inspiration</h2>
         <div>
           <label className="new-card-form__form-label" htmlFor="text">Name:</label>
-          <input name="text" placeholder="text" onChange={this.onFormChange} value={this.state.text} />
+          <input name="text" className="new-card-form__form-textarea" placeholder="text" onChange={this.onFormChange} value={this.state.text} />
         </div>
         <div>
-          <label className="new-card-form__form-label" htmlFor="select">Emoji:</label>
-          <select value={this.state.value} onChange={this.onFormChange}>
+          <label className="new-card-form__form-label" htmlFor="select">Emoji: </label>
+          <select className="new-card-form__form-select" name="emoji" value={this.state.value} onChange={this.onFormChange}>
             {populateOptions}
           </select>
         </div>
-        <input className="btn btn-success new-pet-form--submit" type="submit" name="submit" value="Add a Pet" />
+        <input className="btn btn-success new-card-form__form-button" type="submit" name="submit" value="Submit" />
       </form>
     );
   };
